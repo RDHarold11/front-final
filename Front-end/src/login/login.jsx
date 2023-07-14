48235e77a117f2baf5faae8531e9760a2a0ce989
Raynier Perez Minyety <rayniercraft@gmail.com>
2023-07-14 15:08:22 -0400

    Inicio del login + routes

---
+++ Front-end/src/login/login.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './login.css'
+
+
+function Login() {
+
+    return(<>
+        <div id="Login-window">
+            
+        </div>
+    </>
+    )
+}
+
+export default Login;
