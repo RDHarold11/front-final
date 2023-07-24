913a9c9a1c296ddba908ef5451d1ba139806885b
Harold Manuel <aquinoharold460@gmail.com>
2023-07-24 16:05:37 -0400

    Archivo de articulos recientes

--- Front-end/src/pages/start/start.jsx
+++ Front-end/src/pages/start/start.jsx
@@ -2,11 +2,13 @@ import CardContainer from "../../components/CardContainer/card-container";
 import Navbar from "../../components/Navbar/Navbar";
 import AnunciosContainer from "../../components/CardContainer/AnunciosContainer"
 import Mision from "../../components/MisionVisionFilosofia/Mision";
+import Recientes from "../../components/ArticulosRecientes/Recientes";
 
 function Inicio() {
     return (
         <>
         {/* <Navbar></Navbar> */}
+        {/* <Recientes></Recientes> */}
         <AnunciosContainer></AnunciosContainer>
         <CardContainer></CardContainer>
         {/* <Mision></Mision> */}
