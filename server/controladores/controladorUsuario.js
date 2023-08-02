2a48405896e9391df28221d5171abc9532faac62
Harold Manuel <aquinoharold460@gmail.com>
2023-08-02 11:22:10 -0400

    Ahora el usuario puede hacer login

--- server/controladores/controladorUsuario.js
+++ server/controladores/controladorUsuario.js
@@ -3,6 +3,34 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import asyncHandler from "express-async-handler";
 
+/* const regiterUser = asyncHandler(async (req, res) => {
+  const { user, password } = req.body;
+  if (!user || !password) {
+    res.status(400).json({ msg: "Datos faltantes" });
+  }
+
+  const userExits = await Usuario.findOne({ user: user });
+  if (userExits) {
+    res.status(400).json({ msg: "Este usuario ya existe" });
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const userModel = await Usuario.create({
+    user,
+    password: hashedPassword,
+  });
+
+  if (userModel) {
+    res.status(200).json({
+      _id: userModel.id,
+      user: userModel.user,
+      token: generateToken(userModel.id),
+    });
+  }
+});
+ */
 //Para generar el token
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -14,14 +42,15 @@ const loginUser = asyncHandler(async (req, res) => {
   const { user, password } = req.body;
 
   const User = await Usuario.findOne({ user: user });
-  if (user && (await bcrypt.compare(password, User.password))) {
+  if (User && (await bcrypt.compare(password, User.password))) {
     res.json({
       _id: User.id,
       user: User.user,
       token: generateToken(User.id),
     });
+  } else {
+    res.status(400).json({ msg: "Invalid credentials" });
   }
-  res.status(400).json({ msg: "Invalid credentials" });
 });
 
 const controlador = {
