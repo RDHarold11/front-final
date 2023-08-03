bcd0e0a022f312b9d80379f3d647f02f2a798086
Harold Manuel <aquinoharold460@gmail.com>
2023-08-03 16:15:11 -0400

    Configurando el despligue

--- server/controladores/controladorArticulo.js
+++ server/controladores/controladorArticulo.js
@@ -19,27 +19,29 @@ const postArticle = asyncHandler(async (req, res) => {
   res.status(200).json(article);
 });
 
-const deleteArticles = asyncHandler(async(req,res)=>
-{
-  const article = await Articulo.findById(req.params.id)
+const deleteArticles = asyncHandler(async (req, res) => {
+  const article = await Articulo.findById(req.params.id);
 
   if (!req.user) {
-    res.status(400).json({msg: `no user find`})
+    res.status(400).json({ msg: `no user find` });
   }
   if (!article) {
-    res.status(400).json({msg: `No articles with id: ${req.params.id}`})
+    res.status(400).json({ msg: `No articles with id: ${req.params.id}` });
   }
   if (article.user.toString() == !article.user.id) {
-    res.status(400).json({msg: `unauthorized`})
+    res.status(400).json({ msg: `unauthorized` });
   }
-  await article.deleteOne()
-  res.status(200).json({msg: `user deleted`})
-})
+  await article.deleteOne();
+  res.status(200).json({ msg: `user deleted` });
+});
 
+const getArticles = () => {
+  console.log("Hello World");
+};
 
 const controladorUser = {
   postArticle,
-  deleteArticles
+  deleteArticles,
 };
 
 export default controladorUser;
